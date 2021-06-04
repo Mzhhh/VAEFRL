d3033cdabb5fb38401f46baebf6726a31ce4f1a5
Zihan Mao <46877599+Mzhhh@users.noreply.github.com>
2021-06-04 16:09:09 +0800

    i fucked up

--- train_critic.py
+++ train_critic.py
@@ -32,7 +32,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 parser = argparse.ArgumentParser()
 parser.add_argument("--seed", default=0, type=int)               # Sets Gym, PyTorch and Numpy seeds
 parser.add_argument("--max_timesteps", default=1e6, type=float)    # Max time steps to run environment
-parser.add_argument("--max_episode_steps", default=1e2, type=float)    # Max time steps to run environment
+parser.add_argument("--max_episode_steps", default=1e2, type=float)
 parser.add_argument("--buffer_size", default=1e6, type=float)    # Max time steps to run environment
 parser.add_argument("--expl_noise", default=0.1)                 # Std of Gaussian exploration noise
 parser.add_argument("--batch_size", default=128, type=float)       # Batch size for both actor and critic
