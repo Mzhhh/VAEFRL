21d1ef4a49be56ac7613bf44acbaa230eca8dc40
Zihan Mao <46877599+Mzhhh@users.noreply.github.com>
2021-06-10 14:55:59 +0800

    fix

--- train_critic.py
+++ train_critic.py
@@ -98,7 +98,7 @@ buffer_raw = ReplayBuffer((3, 64, 64), action_dim, REPLAY_BUFFER_SIZE, device=de
 policy_raw = DDPG_CNN.DDPG(3, action_dim, min_action, max_action)
 
 if PRETRAINED_MODEL:
-    policy_raw.load(PRETRAINED_MODEL)
+    policy_raw.load(os.path.join("./model_checkpoints", PRETRAINED_MODEL))
 
 expert_model = CarRacingDQNAgent(epsilon=0)
 expert_model.load("tf_best.h5")
